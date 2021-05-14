9d7f85997f06a1e3227db663e119b993c4063cfb
Mitch <admin@mitchdev.net>
2021-05-14 15:26:30 +1200

    Andlin api ping and optional unit for weather
    Andlin api ping and optional unit for weather

--- commands/info.js
+++ commands/info.js
@@ -5,10 +5,18 @@ exports.slash = {
     description: 'Shows uptime & ping'
 }
 exports.handler = function(message) {
-    var content = `**Client Uptime** ${secondsToDhms(client.uptime/1000)}\n**System Uptime** ${secondsToDhms(os.uptime())}\n\n**Client Ping** ${Date.now() - message.createdTimestamp}ms\n**API Ping** ${Math.round(client.ws.ping)}ms`;
-    if (message.interaction) {
-        message.interaction.editReply(content);
-    } else {
-        message.channel.send(content);
-    }
+    var pre = new Date();
+    request({
+        method: 'POST',
+        url: options.api.andlinPing.url,
+        headers: {'Authorization': options.api.andlinPing.auth}
+    }, (coordinatesErr, coordinatesReq, coordinatesRes) => {
+        var post = new Date();
+        var content = `**Client Uptime** ${secondsToDhms(client.uptime/1000)}\n**System Uptime** ${secondsToDhms(os.uptime())}\n\n**Client Ping** ${Date.now() - message.createdTimestamp}ms\n**Discord API Ping** ${Math.round(client.ws.ping)}ms\n**Andlin API Ping** ${(post - pre)}ms`;
+        if (message.interaction) {
+            message.interaction.editReply(content);
+        } else {
+            message.channel.send(content);
+        }
+    });
 }
--- commands/weather.js
+++ commands/weather.js
@@ -4,6 +4,11 @@ exports.slash = {
     name: 'weather',
     description: 'Gets weather from a location',
     options: [{
+        name: 'units',
+        type: 'STRING',
+        description: 'Unit of measurment (imperial/metric)',
+        required: false
+    }, {
         name: 'location',
         type: 'STRING',
         description: 'Location to get weather from',
@@ -11,117 +16,120 @@ exports.slash = {
     }]
 }
 exports.handler = function(message) {
-	var location = message.content.replace('!weather ', '');
-	if (location != '') {
-        request({
-			method: 'POST',
-			url: options.api.coordinates.url,
-			headers: {'Authorization': options.api.coordinates.auth},
-			json: {"Address": location}
-        }, (coordinatesErr, coordinatesReq, coordinatesRes) => {
-            if (!coordinatesErr) {
-                if (coordinatesRes.Message) {
-                    if (coordinatesRes.Message.startsWith('404 Not Found:')) {
-                        if (message.interaction) {
-                            message.interaction.editReply(`Could not find ${location}`);
+    if (message.content.split(' ').length > 1) {
+        var units = (message.content.toLowerCase().split(' ')[1] === 'imperial') ? 'imperial' : 'metric'
+        var location = message.content.replace('!weather ', '').replace(units, '');
+        if (location != '') {
+            request({
+                method: 'POST',
+                url: options.api.coordinates.url,
+                headers: {'Authorization': options.api.coordinates.auth},
+                json: {"Address": location}
+            }, (coordinatesErr, coordinatesReq, coordinatesRes) => {
+                if (!coordinatesErr) {
+                    if (coordinatesRes.Message) {
+                        if (coordinatesRes.Message.startsWith('404 Not Found:')) {
+                            if (message.interaction) {
+                                message.interaction.editReply(`Could not find ${location}`);
+                            } else {
+                                message.channel.send(`Could not find ${location}`);
+                            }
                         } else {
-                            message.channel.send(`Could not find ${location}`);
+                            client.users.fetch(options.user.mitch).then(mitch => {
+                                mitch.send(`**Coordinates:** ${coordinatesRes.Message}\n**Sent:** \`\`\`{"Address": ${location}}\`\`\``);
+                            });
+                            client.users.fetch(options.user.andlin).then(andlin => {
+                                andlin.send(`**Coordinates:** ${coordinatesRes.Message}\n**Sent:** \`\`\`{"Address": ${location}}\`\`\``);
+                            });
                         }
                     } else {
-                        client.users.fetch(options.user.mitch).then(mitch => {
-                            mitch.send(`**Coordinates:** ${coordinatesRes.Message}\n**Sent:** \`\`\`{"Address": ${location}}\`\`\``);
-                        });
-                        client.users.fetch(options.user.andlin).then(andlin => {
-                            andlin.send(`**Coordinates:** ${coordinatesRes.Message}\n**Sent:** \`\`\`{"Address": ${location}}\`\`\``);
-                        });
-                    }
-                } else {
-                    request(options.api.weather.url + options.api.weather.auth + `&lat=${coordinatesRes.lat}&lon=${coordinatesRes.lon}`, (err, req, res) => {
-                        if (!err) {
-                            var data = JSON.parse(res);
-                            var sunText = "";
-                            var moonText = "";
-                            var rainText = "";
-                            var snowText = "";
-                            var windText = "";
-                            var alertsText = "";
-    
-                            var sunrise = data.daily[0].sunrise - data.current.dt;
-                            var sunset = data.daily[0].sunset - data.current.dt;
-                            if (sunrise <= 5 && sunrise >= -5) sunText = `\n☀️ Sun is rising now`;
-                            else if (sunset <= 5 && sunset >= -5) sunText = `\n☀️ Sun is setting now`;
-                            else if (sunrise >= 0) sunText = `\n☀️ Sun is rising in ${secondsToDhms(sunrise)}`;
-                            else if (sunset >= 0) sunText = `\n☀️ Sun is setting in ${secondsToDhms(sunset)}`;
-                            else sunText = `\n☀️ Sunset ${secondsToDhms(Math.abs(sunset))}ago`;
-    
-                            if (data.daily[0].moon_phase > 0 && data.daily[0].moon_phase < 0.25) moonText = `\n🌒 Waxing crescent moon`;
-                            if (data.daily[0].moon_phase > 0.25 && data.daily[0].moon_phase < 0.5) moonText = `\n🌔 Waxing gibous moon`;
-                            if (data.daily[0].moon_phase > 0.5 && data.daily[0].moon_phase < 0.75) moonText = `\n🌖 Waning gibous moon`;
-                            if (data.daily[0].moon_phase > 0.75 && data.daily[0].moon_phase < 1) moonText = `\n🌘 Waning crescent moon`;
-                            if (data.daily[0].moon_phase == 0 || data.daily[0].moon_phase == 1) moonText = `\n🌑 New moon`;
-                            if (data.daily[0].moon_phase == 0.25) moonText = `\n🌓 First quarter moon`;
-                            if (data.daily[0].moon_phase == 0.5) moonText = `\n🌕 Full moon`;
-                            if (data.daily[0].moon_phase == 0.75) moonText = `\n🌗 Last quarter moon`;
-    
-                            var moonrise = data.daily[0].moonrise - data.current.dt;
-                            var moonset = data.daily[0].moonset - data.current.dt;
-                            if (moonrise <= 5 && moonrise >= -5) moonText += ` is rising now`;
-                            else if (moonset <= 5 && moonset >= -5) moonText += ` is setting now`;
-                            else if (moonrise >= 0) moonText += ` is rising in ${secondsToDhms(moonrise)}`;
-                            else if (moonset >= 0) moonText += ` is setting in ${secondsToDhms(moonset)}`;
-                            else moonText += ` set ${secondsToDhms(Math.abs(moonset))}ago`;
-    
-                            if (data.hourly[0].rain) rainText = `\n🌧️ ${data.hourly[0].rain["1h"]}mm of rain`;
-                            if (data.hourly[0].snow) snowText = `\n🌨️ ${data.hourly[0].snow["1h"]}mm of snow`;
-    
-                            windText = `\n💨 ${(data.hourly[0].wind_speed*3.6).toFixed(2)}km/h (${data.hourly[0].wind_speed}m/s)(${(data.hourly[0].wind_speed * 2.237).toFixed(2)}mi/h) ${getCardinalDirection(data.hourly[0].wind_deg)}`;
-                            if (data.hourly[0].wind_gust) windText += `\n💨 ${(data.hourly[0].wind_gust*3.6).toFixed(2)}km/h (${data.hourly[0].wind_gust}m/s)(${(data.hourly[0].wind_gust * 2.237).toFixed(2)}mi/h) gusts`;
-    
-                            if (data.alerts) {
-                                if (data.alerts.length > 0) {
-                                    alertsText = `${data.alerts.map(alert => {
-                                        var alertTime = "";
-                                        var alertStart = alert.start - data.current.dt;
-                                        var alertEnd = alert.end - data.current.dt;
-                                        var alertDuration = alert.end - alert.start;
-    
-                                        if (alertStart > 5) alertTime = `starts in ${secondsToDhms(alertStart)}and lasts for ${secondsToDhms(alertDuration)}`
-                                        else if (alertEnd > 5) alertTime = `started ${secondsToDhms(Math.abs(alertStart))}ago and ends in ${secondsToDhms(alertEnd)}`
-                                        else alertTime = `ended ${secondsToDhms(Math.abs(alertEnd))}ago and lasted ${secondsToDhms(alertDuration)}`
-    
-                                        return `**⚠️ ${capitalize(alert.event)}** ${alertTime}\n${alert.description}`;
-                                    }).join('\n')}\n\n`;
+                        request(options.api.weather.url + options.api.weather.auth + `&lat=${coordinatesRes.lat}&lon=${coordinatesRes.lon}&units=${units}`, (err, req, res) => {
+                            if (!err) {
+                                var data = JSON.parse(res);
+                                var sunText = "";
+                                var moonText = "";
+                                var rainText = "";
+                                var snowText = "";
+                                var windText = "";
+                                var alertsText = "";
+        
+                                var sunrise = data.daily[0].sunrise - data.current.dt;
+                                var sunset = data.daily[0].sunset - data.current.dt;
+                                if (sunrise <= 5 && sunrise >= -5) sunText = `\n☀️ Sun is rising now`;
+                                else if (sunset <= 5 && sunset >= -5) sunText = `\n☀️ Sun is setting now`;
+                                else if (sunrise >= 0) sunText = `\n☀️ Sun is rising in ${secondsToDhms(sunrise)}`;
+                                else if (sunset >= 0) sunText = `\n☀️ Sun is setting in ${secondsToDhms(sunset)}`;
+                                else sunText = `\n☀️ Sunset ${secondsToDhms(Math.abs(sunset))}ago`;
+        
+                                if (data.daily[0].moon_phase > 0 && data.daily[0].moon_phase < 0.25) moonText = `\n🌒 Waxing crescent moon`;
+                                if (data.daily[0].moon_phase > 0.25 && data.daily[0].moon_phase < 0.5) moonText = `\n🌔 Waxing gibous moon`;
+                                if (data.daily[0].moon_phase > 0.5 && data.daily[0].moon_phase < 0.75) moonText = `\n🌖 Waning gibous moon`;
+                                if (data.daily[0].moon_phase > 0.75 && data.daily[0].moon_phase < 1) moonText = `\n🌘 Waning crescent moon`;
+                                if (data.daily[0].moon_phase == 0 || data.daily[0].moon_phase == 1) moonText = `\n🌑 New moon`;
+                                if (data.daily[0].moon_phase == 0.25) moonText = `\n🌓 First quarter moon`;
+                                if (data.daily[0].moon_phase == 0.5) moonText = `\n🌕 Full moon`;
+                                if (data.daily[0].moon_phase == 0.75) moonText = `\n🌗 Last quarter moon`;
+        
+                                var moonrise = data.daily[0].moonrise - data.current.dt;
+                                var moonset = data.daily[0].moonset - data.current.dt;
+                                if (moonrise <= 5 && moonrise >= -5) moonText += ` is rising now`;
+                                else if (moonset <= 5 && moonset >= -5) moonText += ` is setting now`;
+                                else if (moonrise >= 0) moonText += ` is rising in ${secondsToDhms(moonrise)}`;
+                                else if (moonset >= 0) moonText += ` is setting in ${secondsToDhms(moonset)}`;
+                                else moonText += ` set ${secondsToDhms(Math.abs(moonset))}ago`;
+        
+                                if (data.hourly[0].rain) rainText = `\n🌧️ ${data.hourly[0].rain["1h"]}mm of rain`;
+                                if (data.hourly[0].snow) snowText = `\n🌨️ ${data.hourly[0].snow["1h"]}mm of snow`;
+        
+                                windText = `\n💨 ${(units === 'imperial') ? data.hourly[0].wind_speed+'mi/h' : (data.hourly[0].wind_speed*3.6).toFixed(2)+'km/h'} ${getCardinalDirection(data.hourly[0].wind_deg)}`;
+                                if (data.hourly[0].wind_gust) windText += ` and ${(units === 'imperial') ? data.hourly[0].wind_gust+'mi/h' : (data.hourly[0].wind_gust*3.6).toFixed(2)+'km/h'} gusts`;
+        
+                                if (data.alerts) {
+                                    if (data.alerts.length > 0) {
+                                        alertsText = `${data.alerts.map(alert => {
+                                            var alertTime = "";
+                                            var alertStart = alert.start - data.current.dt;
+                                            var alertEnd = alert.end - data.current.dt;
+                                            var alertDuration = alert.end - alert.start;
+        
+                                            if (alertStart > 5) alertTime = `starts in ${secondsToDhms(alertStart)}and lasts for ${secondsToDhms(alertDuration)}`
+                                            else if (alertEnd > 5) alertTime = `started ${secondsToDhms(Math.abs(alertStart))}ago and ends in ${secondsToDhms(alertEnd)}`
+                                            else alertTime = `ended ${secondsToDhms(Math.abs(alertEnd))}ago and lasted ${secondsToDhms(alertDuration)}`
+        
+                                            return `**⚠️ ${capitalize(alert.event)}** ${alertTime}\n${alert.description}`;
+                                        }).join('\n')}\n\n`;
+                                    }
+                                }
+        
+                                var content =   `${coordinatesRes.manicipality}, ${coordinatesRes.countryCode} has ${data.hourly[0].weather[0].description} (Location confidence: ${coordinatesRes.score}%)\n\n`+
+                                                `${alertsText}`+
+                                                `**---- This Hour ----**\n`+
+                                                `**Currently** ${data.hourly[0].temp}${units === 'imperial' ? '°F' : '°C'}\n`+
+                                                `**Feels like** ${data.hourly[0].feels_like}${units === 'imperial' ? '°F' : '°C'}\n`+
+                                                `**Rain probability** ${data.hourly[0].pop*100}%\n`+
+                                                `${rainText}${snowText}`+
+                                                `\n☁️ ${data.hourly[0].clouds}% cloud cover`+
+                                                `${windText}${sunText}${moonText}\n\n`+
+                                                `**High** ${data.daily[0].temp.max}${units === 'imperial' ? '°F' : '°C'}\n`+
+                                                `**Low** ${data.daily[0].temp.min}${units === 'imperial' ? '°F' : '°C'}\n`+
+                                                `**Dew point** ${data.hourly[0].dew_point}${units === 'imperial' ? '°F' : '°C'}\n`+
+                                                `**Humidity** ${data.hourly[0].humidity}%\n`+
+                                                `**Pressure** ${data.hourly[0].pressure}hPa\n`+
+                                                `**UV** ${data.hourly[0].uvi}\n`+
+                                                `**Visibility** ${units === 'imperial' ? (data.hourly[0].visibility/1609).toFixed(2)+'mi' : (data.hourly[0].visibility/1000).toFixed(2)+'km'}\n`;
+        
+                                if (message.interaction) {
+                                    message.interaction.editReply(content);
+                                } else {
+                                    message.channel.send(content);
                                 }
                             }
-    
-                            var content =   `${coordinatesRes.manicipality}, ${coordinatesRes.countryCode} has ${data.hourly[0].weather[0].description} (Location confidence: ${coordinatesRes.score}%)\n\n`+
-                                            `${alertsText}`+
-                                            `**---- This Hour ----**\n`+
-                                            `**Currently** ${data.hourly[0].temp}°C (${((data.hourly[0].temp * 1.8) + 32).toFixed(2)}°F)\n`+
-                                            `**Feels like** ${data.hourly[0].feels_like}°C (${((data.hourly[0].feels_like * 1.8) + 32).toFixed(2)}°F)\n`+
-                                            `**Rain probability** ${data.hourly[0].pop*100}%\n`+
-                                            `${rainText}${snowText}`+
-                                            `\n☁️ ${data.hourly[0].clouds}% cloud cover`+
-                                            `${windText}${sunText}${moonText}\n\n`+
-                                            `**High** ${data.daily[0].temp.max}°C (${((data.daily[0].temp.max * 1.8) + 32).toFixed(2)}°F)\n`+
-                                            `**Low** ${data.daily[0].temp.min}°C (${((data.daily[0].temp.min * 1.8) + 32).toFixed(2)}°F)\n`+
-                                            `**Dew point** ${data.hourly[0].dew_point}°C (${((data.hourly[0].dew_point * 1.8) + 32).toFixed(2)}°F)\n`+
-                                            `**Humidity** ${data.hourly[0].humidity}%\n`+
-                                            `**Pressure** ${data.hourly[0].pressure}hPa\n`+
-                                            `**UV** ${data.hourly[0].uvi}\n`+
-                                            `**Visibility** ${data.hourly[0].visibility/1000}km (${(data.hourly[0].dew_point / 1.609).toFixed(2)}mi)\n`;
-    
-                            if (message.interaction) {
-                                message.interaction.editReply(content);
-                            } else {
-                                message.channel.send(content);
-                            }
-                        }
-                    });
+                        });
+                    }
                 }
-            }
-        });
-	}
+            });
+        }
+    }
 
 	function getCardinalDirection(deg) {
 		if ((deg > 348.75 && deg <= 360) || (deg < 11.25 && deg >= 0)) return `N`;

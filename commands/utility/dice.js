3cbd50707329ad468113b632794ca913700a2037
Mitch <admin@mitchdev.net>
2021-10-25 00:07:24 +1300

    Improved dice command
    Improved dice command

--- commands/utility/dice.js
+++ commands/utility/dice.js
@@ -18,37 +18,17 @@ exports.slashes = [{
 exports.commandHandler = async function(interaction, Discord) {
   const dice = interaction.options.get('dice').value;
   const sides = interaction.options.get('sides').value;
-  const rolled = [];
-  let sum = 0;
 
-  if (dice > 100 || dice < 1) {
+  if (dice > 10000 || dice < 1 || sides > 1000000 || sides < 1) {
     await interaction.deferReply({ephemeral: true});
-    interaction.editReply({content: 'Maximum of 100 dice, Minimum of 1 dice'});
-  } else if (sides > 1000 || sides < 1) {
-    await interaction.deferReply({ephemeral: true});
-    interaction.editReply({content: 'Maximum of 1000 sides, Minimum of 1 sides'});
+
+    interaction.editReply({content: `Maximum of 10000 dice, Minimum of 1 dice.\nMaximum of 1,000,000 sides, Minimum of 1 sides.`});
   } else {
     await interaction.deferReply();
 
-    for (let i = 0; i < dice; i++) {
-      const roll = Math.round(Math.random() * (sides-1))+1;
-      rolled.push(roll);
-      sum += roll;
-    }
-
-    const embed = new Discord.MessageEmbed().setTitle(`Rolling ${dice} dice of ${sides} sides`);
-
-    embed.addFields([{
-      name: 'Result',
-      value: sum.toString(),
-    }]);
-
-    if (dice > 1) {
-      embed.addFields([{
-        name: 'Dice',
-        value: rolled.join(', '),
-      }]);
-    }
+    const rolled = Array.from({length: dice}, () => Math.floor(Math.random() * (sides-1))+1);
+    const embed = new Discord.MessageEmbed().setTitle(`Rolling ${dice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')} dice of ${sides.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')} sides`);
+    embed.setDescription(rolled.reduce((a, b) => a+b).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','));
 
     interaction.editReply({embeds: [embed]});
   }
